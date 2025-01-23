61466f31f58563abb285a4c40e894529641a30c8
nikita_rayte <raytenikita7@gmail.com>
2025-01-23 09:55:18 +0530

    frontend added

---
+++ dist/assets/Dashboard-82cd91ed.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-0f181910.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const { resturantProtected, customerProtected, adminProtected, riderProtected } = require("./middlewares/Protected")
 const { httpServer, app } = require("./socket/socket")
+const path = require("path")
 
 // const app = express()
 
 app.use(express.json()) // req.body
 app.use(cookieParser()) // req.cookies
+app.use(express.static("dist"))
 app.use(cors({
     origin: true,
     credentials: true // cookie
@@ -21,7 +23,8 @@ app.use("/api/customer", customerProtected, require("./routes/customer.routes"))
 app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
 app.use("/api/rider", riderProtected, require("./routes/rider.routes"))
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "resource not found" })
 })
 app.use((err, req, res, next) => {
     console.log(err)
