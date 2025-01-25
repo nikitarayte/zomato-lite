ebd4a775675f5e50ac5747cfc9b399494cde8764
nikita_rayte <raytenikita7@gmail.com>
2025-01-25 09:32:48 +0530

    delivered status delete

--- controllers/rider.controller.js
+++ controllers/rider.controller.js
@@ -6,7 +6,7 @@ const { io } = require("../socket/socket")
 
 exports.getRiderOrders = asyncHandler(async (req, res) => {
     const result = await Order
-        .find({ rider: req.user })
+        .find({ rider: req.user, status: { $ne: "delivered" } })
         .select("-rider -createdAt -updatedAt -__v")
         .populate("customer", "name mobile address")
         .populate("resturant", "name hero mobile address")
